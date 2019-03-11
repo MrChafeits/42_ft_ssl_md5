ca0570f6ad9bc4d99cc62c01273d616f4fd363fb
Clayton Allen <callen@student.42.us.org>
2019-03-11 16:23:12 -0700

    removing outdated libft

--- old.mk
+++ old.mk
@@ -3,75 +3,65 @@
 #                                                         :::      ::::::::    #
 #    Makefile                                           :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
-#    By: callen <callen@student.42.us.org>          +#+  +:+       +#+         #
+#    By: callen <callen@student.42.fr>              +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2018/09/23 22:04:17 by callen            #+#    #+#              #
-#    Updated: 2019/03/11 16:22:48 by callen           ###   ########.fr        #
+#    Created: 2018/09/16 21:01:37 by callen            #+#    #+#              #
+#    Updated: 2019/02/20 20:02:55 by callen           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 NAME := ft_ssl
-DNAM := d_$(NAME)
-
+DNAM := d_ft_ssl
 CC := clang
 CFLAGS := -Wall -Wextra -Werror
 DFLAGS := -Wall -Wextra -g
 INCDIR := includes/
-INCFLAGS := -I $(INCDIR) -I libft/$(INCDIR)
+INCFLAGS := -I $(INCDIR) -I libft/includes
 LIBFLAGS := -L libft/ -lft
 OBJDIR := .obj/
-SRCDIR := srcs/
-SRC +=	main.c
-
+SRCDIR := src/
+SRC := main.c
 NORME := $(addsuffix *.h,$(INCDIR)) $(addsuffix *.c,$(SRCDIR))
 OBJ := $(addprefix $(OBJDIR), $(SRC:.c=.o))
 
-ASAN := -fsanitize=address
-
-.PHONY: all debug clean dclean fclean re tags libft j k asan norme codesize
+.PHONY: all clean fclean re tags dclean debug norme fsan
 
 all: $(NAME)
 
-libft:
+$(NAME): $(OBJDIR) $(OBJ)
 	@make -s -C libft
-
-$(NAME): libft $(OBJDIR) $(OBJ)
 	@$(CC) $(INCFLAGS) $(LIBFLAGS) -o $(NAME) $(OBJ)
 
-j: debug
+debug: $(addprefix $(SRCDIR), $(SRC))
+	@make -s -C libft
+	@$(CC) $(DFLAGS) $(INCFLAGS) $(LIBFLAGS) -o $(DNAM) $<
 
-k: fclean dclean
-
-asan: libft
-	@$(CC) $(DFLAGS) $(ASAN) $(INCFLAGS) $(LIBFLAGS) -o $(DNAM) $(addprefix $(SRCDIR), $(SRC))
-
-debug: libft
-	@$(CC) $(DFLAGS) $(INCFLAGS) $(LIBFLAGS) -o $(DNAM) $(addprefix $(SRCDIR), $(SRC))
-
-dclean:
-	@rm -rf $(DNAM) $(DNAM).dSYM
+fsan: $(addprefix $(SRCDIR), $(SRC))
+	@make -s -C libft
+	@$(CC) $(DFLAGS) -fsanitize=address $(INCFLAGS) $(LIBFLAGS) -o $(DNAM) $<
 
 clean:
-	@make -s -C libft/ clean
-	@rm -Rf $(OBJDIR)
+	@make -s -C libft clean
+	@/bin/rm -Rf $(OBJDIR)
+
+dclean:
+	@/bin/rm -rf $(DNAM) $(DNAM).dSYM
 
 fclean: clean
-	@make -s -C libft/ fclean
-	@rm -f $(NAME)
+	@make -s -C libft fclean
+	@/bin/rm -f $(NAME)
 
 re: fclean all
 
 tags:
 	@ctags $(addsuffix *.h,$(INCDIR)) $(addsuffix *.c,$(SRCDIR))
 
+norme:
+	@$(call disp_title,Norme,$(NORM_COLOR_T))
+	@norminette $(NORME)
+
 $(addprefix $(OBJDIR), %.o): $(addprefix $(SRCDIR), %.c)
 	@$(CC) $(INCFLAGS) $(CFLAGS) -o $@ -c $<
 
 $(OBJDIR):
 	@mkdir -p $(OBJDIR)
-
-norme:
-	@norminette $(NORME)
-
-codesize:
-	@cat $(NORME) | ggrep -v '/\*' | wc -l
